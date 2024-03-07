9a7bdff34257d27696f9ea310d2144105a12a537
miawithcode <cuttontail@gmail.com>
2024-03-07 17:26:46 +0800

    feat: create layouts and pages

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+const Page = () => {
+  return (
+    <div>Invoices Page</div>
+  )
+}
+export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+const Page = () => {
+  return <div>Dashboard Page</div>;
+};
+export default Page;
